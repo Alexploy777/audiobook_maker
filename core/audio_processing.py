216a6b80d9507e45ee8dbf397c3f4cb60354bc68
AlexItalbazar <a-egorov@bk.ru>
2024-09-17 16:07:15 +0300

    Приступаем к функции "def convert_and_combine"

--- core/audio_processing.py
+++ core/audio_processing.py
@@ -5,3 +5,7 @@ from pydub import AudioSegment
 class AudioProcessor:
     def __init__(self, ffmpeg_path):
         AudioSegment.converter = ffmpeg_path
+
+
+    def convert_and_combine(self, file_paths, output_path, bitrate, metadata, update_progress):
+        pass
