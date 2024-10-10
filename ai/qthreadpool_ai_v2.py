df5202ab18b5f772d114b38f0d8387f3d83165fd
AlexItalbazar <a-egorov@bk.ru>
2024-10-10 11:33:19 +0300

    Создали новую версию qthreadpool_ai_v2
    оптимизировал версию

--- ai/qthreadpool_ai_v2.py
+++ ai/qthreadpool_ai_v2.py
@@ -8,21 +8,35 @@ os.environ['PATH'] += os.pathsep + os.path.abspath('external')
 
 
 class Converter(QRunnable):
-    def __init__(self, input_path, output_dir_name, output_list, index):
+    def __init__(self, file, output_temp_file, output_temp_files_list, index):
         super().__init__()
-        self.input_path = input_path
-        self.output_dir_name = output_dir_name
-        self.output_list = output_list
+        self.input_path = file
+        self.output_temp_file = output_temp_file
+        self.output_temp_files_list = output_temp_files_list
         self.index = index
 
     @pyqtSlot()
     def run(self):
-        output_file = self.convert_mp3_to_m4b(self.input_path)
-        self.output_list[self.index] = output_file  # Запись результата по индексу
+        output_file = self.convert_mp3_to_m4b(self.input_path, self.output_temp_file)
+        self.output_temp_files_list[self.index] = output_file  # Запись результата по индексу
+
+    def convert_mp3_to_m4b(self, input_path, output_path):
+        audio = AudioSegment.from_mp3(input_path)
+        # output_full_path = self.get_output_file_path(input_path)
+        audio.export(output_path, format="mp4", codec="aac")
+        print(f"Файл успешно конвертирован: {output_path}")
+        return output_path
+
+
+class ConverterManager:
+    def __init__(self, output_dir_name):
+        self.output_dir_name = output_dir_name
+        self.thread_pool = QThreadPool()
+        self.output_temp_files_list = []  # Список для хранения путей к выходным файлам
 
     def get_output_file_path(self, input_path):
         '''
-        Функция формирует и возвращает абсолютный путь временного файла с именем исходного файла
+        Функция формирует и возвращает абсолютный путь временного файла с исходным именем
         :param input_path: str
         :return: str
         '''
@@ -34,24 +48,11 @@ class Converter(QRunnable):
         output_name = os.path.join(str(full_output_dir), name + '.m4b')
         return output_name
 
-    def convert_mp3_to_m4b(self, input_path):
-        audio = AudioSegment.from_mp3(input_path)
-        output_full_path = self.get_output_file_path(input_path)
-        audio.export(output_full_path, format="mp4", codec="aac")
-        print(f"Файл успешно конвертирован: {output_full_path}")
-        return output_full_path
-
-
-class ConverterManager:
-    def __init__(self, output_dir_name):
-        self.output_dir_name = output_dir_name
-        self.thread_pool = QThreadPool()
-        self.output_temp_files = []  # Список для хранения путей к выходным файлам
-
     def start(self, input_list):
-        self.output_temp_files = [None] * len(input_list)  # Инициализируем список с None для каждого файла
+        self.output_temp_files_list = [None] * len(input_list)  # Инициализируем список с None для каждого файла
         for index, file in enumerate(input_list):
-            converter = Converter(file, self.output_dir_name, self.output_temp_files, index)
+            output_temp_file = self.get_output_file_path(file)
+            converter = Converter(file, output_temp_file, self.output_temp_files_list, index)
             self.thread_pool.start(converter)
 
         # Ждём завершения всех потоков
@@ -80,7 +81,7 @@ if __name__ == '__main__':
 
     converter_manager = ConverterManager(output_dir_name)
     converter_manager.start(input_list)
-    temp_files_list = converter_manager.output_temp_files
+    temp_files_list = converter_manager.output_temp_files_list
 
     combine = Combine(output_file)
     combine.combine_files(temp_files_list)
