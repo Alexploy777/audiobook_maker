f996583d716e0f75928e3634cb8acbf38c89d419
AlexItalbazar <a-egorov@bk.ru>
2024-11-05 12:46:36 +0300

    Перенесен в отдельный модуль Converter

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 # core/__init__.py
-from .audio_manager import M4bMerger, Converter
+from .audio_manager import M4bMerger
 from .metadata import MetadataManager
 from .addchapters import AddChapters
 from .convertersignals import ConverterSignals
+from .converter import Converter
--- core/audio_manager.py
+++ core/audio_manager.py
@@ -149,68 +149,68 @@ class M4bMerger(QRunnable):  # Потом убрать!
 #                 os.remove(output_temp_file)
 
 
-class Converter(QRunnable):
-    # class Converter:
-    def __init__(self, index, quantity, file, output_temp_files_list, bitrate):
-        super().__init__()
-        self.index = index
-        self.quantity = quantity
-        self.my_signals = ConverterSignals()
-        self.file = file
-        self.output_temp_files_list = output_temp_files_list
-        self.bitrate = bitrate
-
-        self.audio_codec = Config.AUDIO_CODEC
-        self.output_format = '.' + Config.OUTPUT_FORMAT
-
-    @pyqtSlot()
-    def run(self):
-        """Запускает выполнение задания."""
-        output_file = self.convert_mp3_to_m4b(self.file)
-
-        self.output_temp_files_list[self.index] = output_file
-        self.my_signals.progress_bar_signal.emit(self.index)  # Отправляем сигнал о завершении задания
-        self.my_signals.label_info_signal.emit(f'сконвертирован файл:')
-        self.my_signals.label_info_signal_2.emit(f'{os.path.abspath(self.file)}')
-
-    def convert_mp3_to_m4b(self, input_path):
-        try:
-            # Проверка, существует ли файл
-            if not os.path.isfile(input_path):
-                print(f"Файл не найден: {input_path}")
-                return None
-
-            # Создаём временный файл
-            output_buffer = tempfile.NamedTemporaryFile(suffix=self.output_format, delete=False)
-            output_buffer.close()  # Закрываем, чтобы FFmpeg мог записать в него
-
-            # Команда для FFmpeg ffmpeg -i input.mp3 -vn -c:a aac output.m4b
-            print(self.bitrate)
-            command = [
-                'ffmpeg', '-i', input_path, '-vn', '-c:a', self.audio_codec, '-b:a', self.bitrate, '-y',
-                # -y: перезаписываем файл, если существует
-                output_buffer.name
-            ]
-            # Запускаем FFmpeg и выводим ошибки при необходимости
-            process = subprocess.run(
-                command,
-                creationflags=subprocess.CREATE_NO_WINDOW,
-                capture_output=True,
-                text=True,
-                encoding='utf-8',  # Добавляем utf-8 кодировку для безопасного чтения
-                errors='ignore'  # Игнорируем недопустимые символы, чтобы избежать UnicodeDecodeError
-            )
-
-            if process.returncode != 0:  # Проверяем успешность выполнения
-                print(f"Ошибка FFmpeg: {process.stderr}")
-                return None
-
-            print(f"Файл успешно конвертирован: {input_path}")
-            return output_buffer
-
-        except Exception as e:
-            print(f"Ошибка при конвертации файла {input_path}: {e}")
-            return None
+# class Converter(QRunnable):
+#     # class Converter:
+#     def __init__(self, index, quantity, file, output_temp_files_list, bitrate):
+#         super().__init__()
+#         self.index = index
+#         self.quantity = quantity
+#         self.my_signals = ConverterSignals()
+#         self.file = file
+#         self.output_temp_files_list = output_temp_files_list
+#         self.bitrate = bitrate
+#
+#         self.audio_codec = Config.AUDIO_CODEC
+#         self.output_format = '.' + Config.OUTPUT_FORMAT
+#
+#     @pyqtSlot()
+#     def run(self):
+#         """Запускает выполнение задания."""
+#         output_file = self.convert_mp3_to_m4b(self.file)
+#
+#         self.output_temp_files_list[self.index] = output_file
+#         self.my_signals.progress_bar_signal.emit(self.index)  # Отправляем сигнал о завершении задания
+#         self.my_signals.label_info_signal.emit(f'сконвертирован файл:')
+#         self.my_signals.label_info_signal_2.emit(f'{os.path.abspath(self.file)}')
+#
+#     def convert_mp3_to_m4b(self, input_path):
+#         try:
+#             # Проверка, существует ли файл
+#             if not os.path.isfile(input_path):
+#                 print(f"Файл не найден: {input_path}")
+#                 return None
+#
+#             # Создаём временный файл
+#             output_buffer = tempfile.NamedTemporaryFile(suffix=self.output_format, delete=False)
+#             output_buffer.close()  # Закрываем, чтобы FFmpeg мог записать в него
+#
+#             # Команда для FFmpeg ffmpeg -i input.mp3 -vn -c:a aac output.m4b
+#             print(self.bitrate)
+#             command = [
+#                 'ffmpeg', '-i', input_path, '-vn', '-c:a', self.audio_codec, '-b:a', self.bitrate, '-y',
+#                 # -y: перезаписываем файл, если существует
+#                 output_buffer.name
+#             ]
+#             # Запускаем FFmpeg и выводим ошибки при необходимости
+#             process = subprocess.run(
+#                 command,
+#                 creationflags=subprocess.CREATE_NO_WINDOW,
+#                 capture_output=True,
+#                 text=True,
+#                 encoding='utf-8',  # Добавляем utf-8 кодировку для безопасного чтения
+#                 errors='ignore'  # Игнорируем недопустимые символы, чтобы избежать UnicodeDecodeError
+#             )
+#
+#             if process.returncode != 0:  # Проверяем успешность выполнения
+#                 print(f"Ошибка FFmpeg: {process.stderr}")
+#                 return None
+#
+#             print(f"Файл успешно конвертирован: {input_path}")
+#             return output_buffer
+#
+#         except Exception as e:
+#             print(f"Ошибка при конвертации файла {input_path}: {e}")
+#             return None
 
 
 if __name__ == '__main__':
