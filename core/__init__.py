504422758d130b474a67c5a060010ac8a194eab2
AlexItalbazar <a-egorov@bk.ru>
2024-11-05 12:32:56 +0300

    Переименовал модуль в audio_manager

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 # core/__init__.py
-from .audio_processing import M4bMerger, Converter
+from .audio_manager import M4bMerger, Converter
 from .metadata import MetadataManager
 from .addchapters import AddChapters
 from .convertersignals import ConverterSignals
