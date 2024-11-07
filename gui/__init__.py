a31e234866894884d2e8691138f9ecec7443a80a
AlexItalbazar <a-egorov@bk.ru>
2024-11-07 12:33:56 +0300

    удалил лишние gui

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,6 @@
 # gui/__init__.py
 
-from .gui_test import Ui_MainWindow
+from .gui import Ui_MainWindow
 from .custom_list_widget import CustomListWidget
 from .replacewidget import WidgetReplacer  # , replace_widget
 from .tableviewmanager import TableViewManager
